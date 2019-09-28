ed5ff035e3b2df16a793dac05ed4a6a38732374a
Anton Shumikhin <shumih.anton@gmail.com>
2019-09-29 02:11:01 +0300

    fix start page

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 
-export let pageStorageItem = writable("russian");
+export let pageStorageItem = writable("initial");
 export let addressStorageItem = writable("");
